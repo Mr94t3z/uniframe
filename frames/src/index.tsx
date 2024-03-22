4da115722b92b64759f8cd54e254f6a46deac53e
Muhamad Taopik <mr.94t3z@gmail.com>
2024-03-22 23:14:25 +0700

    💻 add middleware

--- frames/src/index.tsx
+++ frames/src/index.tsx
@@ -11,20 +11,20 @@ export const app = new Frog({
   // basePath: '/poap',
 })
 
-app.use(async (c, next) => { // [!code focus]
-  await next(); // [!code focus]
+app.use(async (c, next) => {
+  await next();
   const isFrame = c.res.headers.get('content-type')?.includes('html');
 
-  if (isFrame) { // [!code focus]
-    let html = await c.res.text(); // [!code focus]
-    const metaTag = '<meta property="of:accepts:xmtp" content="2024-02-01" />'; // [!code focus]
-    html = html.replace(/(<head>)/i, `$1${metaTag}`); // [!code focus]
-    c.res = new Response(html, { // [!code focus]
-      headers: { // [!code focus]
-        'content-type': 'text/html', // [!code focus]
-      }, // [!code focus]
-    }); // [!code focus]
-  } // [!code focus]
+  if (isFrame) {
+    let html = await c.res.text();
+    const metaTag = '<meta property="of:accepts:xmtp" content="2024-02-01" />';
+    html = html.replace(/(<head>)/i, `$1${metaTag}`);
+    c.res = new Response(html, {
+      headers: {
+        'content-type': 'text/html',
+      },
+    });
+  }
 }); 
 
 app.route('/poap', poap)
